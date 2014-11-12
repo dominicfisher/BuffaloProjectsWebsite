cabf935c3d43daa44758aa4f8b548eb97f39366c
buffaloproject1 <chrisfisher236@gmail.com>
2014-11-12 15:55:34 -0600

    Refresh1

--- views/public/js/config.js
+++
@@ -1,8 +0,0 @@
-window.config = {
-	role: 'arn:aws:iam::203816133875:role/buffaloprojects-s3user',
-	principal: 'arn:aws:iam::203816133875:saml-provider/auto0-buffaloprojects-provider',
-	domain:'buffaloprojects.auth0.com',
-	clientID: 'iGcC29FY463ceuL7OUNxwv1LUTQieXkn',
-	targetClientId: 'iGcC29FY463ceuL7OUNxwv1LUTQieXkn',
-	region: 'us-east-1'
-}
